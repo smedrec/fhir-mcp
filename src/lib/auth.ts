70665e2a68b7674c0be8562453910d1808a0f156
José Cordeiro <joseantcordeiro@gmail.com>
2025-06-28 09:08:13 +0100

    refactor: Improve private key handling in JWT authorization

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,13 @@
 import jwt from "jsonwebtoken";
 import base64url from "base64-url";
 import crypto from "crypto";
+import fs from "fs";
 
 // The (last known) access token is stored in this global variable. When it
 
 import { getEnvConfig } from "../mcp/environment.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 interface TokenResponse {
   access_token: string;
@@ -13,12 +16,19 @@ interface TokenResponse {
   scope?: string;
 }
 
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
 /**
  * Authorizes the app and resolves the promise with the access token response
  * @returns {Promise<Object>}
  */
 export async function authorize(): Promise<TokenResponse> {
   const config = getEnvConfig();
+
+  console.log(__dirname)
+
+  const privateKey = fs.readFileSync(path.join(__dirname,'private-key.pem'));
 
   const jwtToken = {
     iss: config.smartIss,
@@ -36,7 +46,7 @@ export async function authorize(): Promise<TokenResponse> {
       "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
     client_assertion: jwt.sign(
       jwtToken,
-      base64url.decode(config.smartPrivateKey.replace(/\\n/g, '\n')),
+      base64url.decode(privateKey.toString()),
       { algorithm: "RS256" }
     ),
   };
